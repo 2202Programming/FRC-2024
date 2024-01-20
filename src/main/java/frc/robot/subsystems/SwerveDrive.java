fb0fcb76597bb7cbe1a71c30a157a34a3fca1eee
elena <elena.simone.rickert.2007@gmail.com>
2024-01-20 14:59:42 -0600

    added drivetrain CAN IDs and Lights subsystem

--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -8,7 +8,26 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SwerveDrive extends SubsystemBase {
   /** Creates a new SwerveDrive. */
-  public SwerveDrive() {}
+  public SwerveDrive() {
+    /* 1/20/24
+     * CAN IDs:
+     * Corner 3 Drive Motor BL: 22
+     * Corner 3 Direction Motor BL: 23
+     * BL Encoder: 28
+     * 
+     * Corner 2 Drive Motor FL: 20
+     * Corner 2 Direction Motor FL: 21
+     * FL Encoder: 29
+     * 
+     * Corner 4 Drive Motor BR: 24
+     * Corner 4 Direction Motor BR: 25
+     * BR Encoder
+     * 
+     * Corner 1 Direction Motor FR: 27
+     * Corner 1 Drive Motor FR: 26
+     * FR Encoder: 30
+     */
+  }
 
   @Override
   public void periodic() {
