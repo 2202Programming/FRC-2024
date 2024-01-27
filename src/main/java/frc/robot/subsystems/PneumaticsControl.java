a1745fb4a45e358e9d21c9f2f658218a38e6eb2e
Kamith <kvml96@gmail.com>
2024-01-27 12:45:47 -0600

    Pneumatics Controller Subsystem
    Bare-bones with comments

---
+++ src/main/java/frc/robot/subsystems/PneumaticsControl.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.ctre.phoenix.led.Animation;
+import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.CANdleConfiguration;
+import com.ctre.phoenix.led.StrobeAnimation;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+import frc.robot.util.RobotSpecs.RobotNames;
+
+public class PneumaticsControl {
+    /** Creates a new Pneumatics Pressure Controller. */
+    public PneumaticsControl() {
+      
+    }
+
+    /*
+    Todo:
+        Create Functions - What functionality is required for this class?
+        Whats are the return type?
+        Whats are the parameters?
+    */
+}
