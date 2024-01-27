caf596c627b59fad88a519d6c16f47881263e940
Jason Rubenstein <jruben4@yahoo.com>
2024-01-27 11:10:20 -0600

    pathing debug tools

--- src/main/java/frc/robot/subsystems/Swerve/SwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/Swerve/SwerveDrivetrain.java
@@ -413,7 +413,6 @@ public class SwerveDrivetrain extends SubsystemBase {
     return modules[modID];
   }
 
-
   public void setZeroPose() {
     setPose(new Pose2d(new Translation2d(0, 0), new Rotation2d(0)));
   }
