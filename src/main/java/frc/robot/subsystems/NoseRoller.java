23d8f71af4d01b9a567c157b154cf8efba281c97
DerekL <derek@adminnet.us>
2024-02-07 17:38:34 -0600

    imports, format, rebase onto master

--- src/main/java/frc/robot/subsystems/NoseRoller.java
+++ src/main/java/frc/robot/subsystems/NoseRoller.java
@@ -6,30 +6,29 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CAN;
+import frc.robot.Constants.DigitalIO;
+import frc.robot.commands.utility.WatcherCmd;
 import frc.robot.util.NeoServo;
 import frc.robot.util.PIDFController;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.DigitalInput;
-import frc.robot.Constants.DigitalIO;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.commands.utility.WatcherCmd;
 
 public class NoseRoller extends SubsystemBase {
   /** Creates a new NoseRoller. */
-    final NeoServo noseAngleMotor;
-    public CANSparkMax noseFireMotor = new CANSparkMax(CAN.NOSE_MOTOR_FIRE, CANSparkMax.MotorType.kBrushless);
-    final PIDFController hwVelPID = new PIDFController(1.0, 0.0, 0.0, 0.0); // inner (hw/vel)
-    final PIDController posPID = new PIDController(1.0,0.0, 0.0);
-    final DigitalInput lightgate = new DigitalInput(DigitalIO.RollerLightGate);
-    public double initialPosition; //Should be our "0" where the nose roller's rollers are in the poition to take the piece
-    public double noseMaxSpeed; //RPM
-  
-   
+  final NeoServo noseAngleMotor;
+  public CANSparkMax noseFireMotor = new CANSparkMax(CAN.NOSE_MOTOR_FIRE, CANSparkMax.MotorType.kBrushless);
+  final PIDFController hwVelPID = new PIDFController(1.0, 0.0, 0.0, 0.0); // inner (hw/vel)
+  final PIDController posPID = new PIDController(1.0, 0.0, 0.0);
+  final DigitalInput lightgate = new DigitalInput(DigitalIO.RollerLightGate);
+  public double initialPosition; // Should be our "0" where the nose roller's rollers are in the poition to take
+                                 // the piece
+  public double noseMaxSpeed; // RPM
+
   public NoseRoller() {
     noseAngleMotor = new NeoServo(CAN.NOSE_MOTOR_ANGLE, posPID, hwVelPID, false);
 
@@ -38,7 +37,8 @@ public class NoseRoller extends SubsystemBase {
   public void setRollerSpeed(double speed) {
     noseFireMotor.set(speed);
   }
-  public boolean rollerHasNote(){
+
+  public boolean rollerHasNote() {
     return lightgate.get();
   }
 
@@ -58,7 +58,7 @@ public class NoseRoller extends SubsystemBase {
     noseFireMotor.set(0.0);
   }
 
-  public boolean isGateBlocked() {    // if we even have a gate?
+  public boolean isGateBlocked() { // if we even have a gate?
     return false;
   }
 
@@ -73,7 +73,7 @@ public class NoseRoller extends SubsystemBase {
   }
 
   /*
-   * Watcher for this subsystem - schedule this command if you need to 
+   * Watcher for this subsystem - schedule this command if you need to
    * see valuse on the Dashboard.
    */
   class NoseRollerWatcherCmd extends WatcherCmd {
@@ -99,6 +99,6 @@ public class NoseRoller extends SubsystemBase {
       // example maxArbFF = nt_maxArbFF.getDouble(maxArbFF);
 
     }
-  }  //watcher command
+  } // watcher command
 
 } // noseroller
