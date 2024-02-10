7035c03bd230196709bb996de2db5d022eb3ca1b
Noah Ren <nren2025@gmail.com>
2024-02-10 11:58:54 -0600

    base command (without merge/checks)

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,29 +2,33 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+//TODO: Add pneumatics/servo to change angle of shooter
+
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkBase.ControlType;
-
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CAN;
+import frc.robot.commands.utility.WatcherCmd;
 import frc.robot.util.PIDFController;
 
 public class Shooter extends SubsystemBase {
 
   static final double FACTOR = 1.0; // TODO put real value here
 
-  public enum ShooterMode {
+  public enum ShooterMode { 
     Trigger, RPM
   };
 
   // Instantiations
   final CANSparkMax leftMtr = new CANSparkMax(CAN.SHOOTER_L, CANSparkMax.MotorType.kBrushless);
   final CANSparkMax rightMtr = new CANSparkMax(CAN.SHOOTER_R, CANSparkMax.MotorType.kBrushless);
-
+  double desiredMotorRPM;
   final SparkPIDController hw_leftPid;
   final SparkPIDController hw_rightPid;
 
@@ -72,6 +76,9 @@ public class Shooter extends SubsystemBase {
       currentShootingMode = ShooterMode.Trigger;
     return;
   }
+  public boolean isAtRPM(){
+    return Math.abs(desiredMotorRPM - getLeftMotorOutput()) < 0.1; // find the method to do it, hack for now
+  }
 
   public ShooterMode getShooterMode() {
     return currentShootingMode;
@@ -83,6 +90,7 @@ public class Shooter extends SubsystemBase {
   public void setRPM(double leftRPM, double rightRPM) {
     hw_leftPid.setReference(leftRPM, ControlType.kVelocity);
     hw_rightPid.setReference(rightRPM, ControlType.kVelocity);
+    desiredMotorRPM = leftRPM;
     // more test code
     // System.out.println("Motor goals changed,
     // left="+leftRPM*gearboxRatio+", right="+rightRPM*gearboxRatio);
@@ -95,6 +103,9 @@ public class Shooter extends SubsystemBase {
 
   public double getRightMotorOutput() {
     return currentRightMotorOutput;
+  }
+  public double getDesiredRPM(){
+    return desiredMotorRPM;
   }
 
 
@@ -150,5 +161,33 @@ public class Shooter extends SubsystemBase {
     enc.setPositionConversionFactor(FACTOR);
     enc.setVelocityConversionFactor(FACTOR / 60.0);
     return enc;
+  }
+  // Network tables
+  class ShooterWatcherCmd extends WatcherCmd {
+    NetworkTableEntry nt_desiredMotorRPM;
+    NetworkTableEntry nt_currentMotorRPM;
+    NetworkTableEntry nt_kP;
+    NetworkTableEntry nt_kI;
+    NetworkTableEntry nt_kD;
+    // add nt for pos when we add it
+    @Override
+    public String getTableName(){
+      return Shooter.this.getName();
+    }
+    public void ntcreate(){
+      NetworkTable table = getTable();
+      nt_desiredMotorRPM = table.getEntry("desiredMotorRPM");
+      nt_currentMotorRPM = table.getEntry("currentMotorRPM");
+      nt_kP = table.getEntry("nt_kP");
+      nt_kI = table.getEntry("nt_kI");
+      nt_kD = table.getEntry("nt_kD");
+    }
+    public void ntupdate(){
+      nt_desiredMotorRPM.setDouble(getDesiredRPM());
+      nt_currentMotorRPM.setDouble(getMotorRPM());
+      nt_kP.setDouble(getP());
+      nt_kI.setDouble(getI());
+      nt_kD.setDouble(getD());
+    }
   }
 }
