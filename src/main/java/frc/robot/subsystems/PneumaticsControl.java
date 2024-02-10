3f87e0b3f086ac3931a6be806396a57d60d5f257
Jason <jruben4@yahoo.com>
2024-02-10 16:04:51 -0600

    overloads/cleanup of pneumatics

--- src/main/java/frc/robot/subsystems/PneumaticsControl.java
+++ src/main/java/frc/robot/subsystems/PneumaticsControl.java
@@ -4,9 +4,7 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PneumaticHub;
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CAN;
@@ -14,20 +12,17 @@ import frc.robot.Constants.CAN;
 public class PneumaticsControl extends SubsystemBase {
 
     // instance variables
-    Compressor compressor;
-    Solenoid m_solenoid;
     PneumaticHub m_PneumaticHub;
 
     private final int PRESSURE_SENSOR1 = 0; //analong pressure sensor
     private double minPressure = 70;
     private double maxPressure = 120;
-    /** Creates a new Pneumatics Pressure Controller. */
+
+    /** Creates a new Pneumatics Controller. */
     public PneumaticsControl() {
         // NOTE
         // new PneumaticsControlModule(CAN.PCM1) causes CAN token errors
         m_PneumaticHub = new PneumaticHub(CAN.PCM1);
-        //m_solenoid = new Solenoid(CAN.PCM1, PneumaticsModuleType.REVPH, 6);
-        //compressor = new Compressor(CAN.PCM1, PneumaticsModuleType.REVPH);
         compressor_on();
     }
 
@@ -40,12 +35,20 @@ public class PneumaticsControl extends SubsystemBase {
         return m_PneumaticHub.getPressure(channel);
     }
 
+    public double get_tank_pressure() {
+        return m_PneumaticHub.getPressure(PRESSURE_SENSOR1);
+    }
+
     public boolean get_compressor_status() {
         return m_PneumaticHub.getCompressor();
     }
 
     public void compressor_on() {
         m_PneumaticHub.enableCompressorAnalog(minPressure,maxPressure);
+    }
+
+    public void compressor_on(double min, double max) {
+        m_PneumaticHub.enableCompressorAnalog(min,max);
     }
 
     public void compressor_off() {
