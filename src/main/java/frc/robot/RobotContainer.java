d81035b7c0669ffc988c4b4399be0a3e29ae0336
Komei Okamoto <komeiokamoto@outlook.jp>
2024-02-19 22:20:35 -0600

    Auto Shooting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -146,7 +147,7 @@ public class RobotContainer {
     autoChooser = AutoBuilder.buildAutoChooser();
 
     NamedCommands.registerCommand("pickup", new IntakeSequence());
-    NamedCommands.registerCommand("shoot", new ShooterSequence(3500.0));
+    NamedCommands.registerCommand("shoot", new ParallelCommandGroup(new ShooterSequence(true,2000), new WaitCommand(2.0)));
     SmartDashboard.putData("Auto Chooser", autoChooser);
   }
 
