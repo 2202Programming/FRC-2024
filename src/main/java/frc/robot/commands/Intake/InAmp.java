ee0c98de575eccc7baa58c417bcebc48f39824dc
Noah Ren <nren2025@gmail.com>
2024-02-23 20:08:50 -0600

    in amp

---
+++ src/main/java/frc/robot/commands/Intake/InAmp.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Intake;
+
+public class InAmp extends Command {
+  final Intake intake;
+  /** Creates a new InAmp. */
+  public InAmp() {
+    this.intake = RobotContainer.getSubsystem(Intake.class);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    intake.setMaxVelocity(30.0);
+    intake.setAngleSetpoint(10.0); //placeholder - find value that works
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(intake.angleAtSetpoint() /* and hits wall */){
+      intake.setIntakeSpeed(-1.0);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    //add max vel?
+    intake.setAngleSetpoint(0.0);
+    intake.setIntakeSpeed(0.0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
