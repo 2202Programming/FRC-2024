adb395f5b2d429e71994cc98ded06609c592301e
Beast <beastsoftware2202@gmail.com>
2024-03-11 21:01:27 -0500

    try again at rotation fix.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -115,10 +115,10 @@ public final class Constants {
 
     // TODO: confirm this when start working on 2024 bot Beta
     public static final ChassisInversionSpecs comp2024BotBetaInversionSpecs = new ChassisInversionSpecs(
-        new ModuleInversionSpecs(true, true, false), // FR
-        new ModuleInversionSpecs(false, true, false), // FL
-        new ModuleInversionSpecs(true, true, false), // BR
-        new ModuleInversionSpecs(false, true, false)); // BL
+        new ModuleInversionSpecs(true, false, false), // FR
+        new ModuleInversionSpecs(false, false, false), // FL
+        new ModuleInversionSpecs(true, false, false), // BR
+        new ModuleInversionSpecs(false, false, false)); // BL
 
     public static final ChassisInversionSpecs swerveBotChassisInversionSpecs = new ChassisInversionSpecs(
         new ModuleInversionSpecs(true, false, false), // FR
