be61c08647e23c2f10c31a775754db02b26c0a56
Beast Software <beastsoftwaredev@gmail.com>
2024-03-15 13:56:02 -0500

    dpl speed to 23fps, current to 40

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -39,12 +39,12 @@ public final class Constants {
     // motor constraints
     public static final double motorMaxRPM = 5600; // motor limit
     //see https://docs.revrobotics.com/sparkmax/software-resources/configuration-parameters
-    public static final int driveStallAmp = 30;
+    public static final int driveStallAmp = 40;  //dpl 3/15 was 30
     public static final int angleStallAmp = 20;
     public static final int freeAmp = 20;
 
     // Constraints on speeds enforeced in DriveTrain
-    public static final double kMaxSpeed = 21.0 * MperFT; // [m/s]
+    public static final double kMaxSpeed = 23.0 * MperFT; // [m/s]  // dpl 3/15 was 21
     public static final double kMaxAngularSpeed = 2 * Math.PI; // [rad/s]
     // TODO: FROM LAST YEAR, NEED TO REVIEW
     /****
