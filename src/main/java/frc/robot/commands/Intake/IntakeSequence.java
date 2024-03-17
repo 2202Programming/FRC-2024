3ac505a31eb226ac2616f3784a6c9ad6d8eed855
Beast Software <beastsoftwaredev@gmail.com>
2024-03-17 12:57:59 -0500

    End of Wisconsin Regional

--- src/main/java/frc/robot/commands/Intake/IntakeSequence.java
+++ src/main/java/frc/robot/commands/Intake/IntakeSequence.java
@@ -69,7 +69,7 @@ public class IntakeSequence extends Command {
           intake.setAngleSetpoint(91.0);
         }
         else{
-        intake.setAngleSetpoint(100.0);
+        intake.setAngleSetpoint(105.0);
         }
         intake.setIntakeSpeed(0.75); // %
         transfer.setSpeed(50.0);
