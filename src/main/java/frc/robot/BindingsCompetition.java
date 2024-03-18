2082e2d2ece03bc6d033bfd317768cf456ff4a20
DerekL <derek@adminnet.us>
2024-03-18 00:38:41 -0500

    Removed some cmd, in favor of instantCmd

--- src/main/java/frc/robot/BindingsCompetition.java
+++ src/main/java/frc/robot/BindingsCompetition.java
@@ -1,16 +1,15 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.Climber.Climb;
-import frc.robot.commands.Climber.ClimbVel;
-import frc.robot.commands.Climber.ClimberCalibrate;
+import frc.robot.commands.Climber.ClimberVelocity;
 import frc.robot.commands.Intake.AngleCalibration;
 import frc.robot.commands.Intake.EjectNote;
 import frc.robot.commands.Intake.InIntake;
 import frc.robot.commands.Intake.IntakeSequence;
 import frc.robot.commands.Intake.MoveToAnglePos;
 import frc.robot.commands.Intake.TestIntake;
-import frc.robot.commands.Shooter.CalibrateSetZero;
 import frc.robot.commands.Shooter.ShooterAngleSetPos;
 import frc.robot.commands.Shooter.ShooterAngleVelMove;
 import frc.robot.commands.Shooter.ShooterServoSequence;
@@ -18,8 +17,9 @@ import frc.robot.commands.Swerve.AllianceAwareGyroReset;
 import frc.robot.commands.Swerve.RobotCentricDrive;
 import frc.robot.commands.auto.AutoShooting;
 import frc.robot.commands.auto.AutoShooting.ShootingTarget;
+import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Climber.Climber;
+import frc.robot.subsystems.ShooterServo;
 import frc.robot.subsystems.Swerve.SwerveDrivetrain;
 import frc.robot.subsystems.hid.HID_Xbox_Subsystem;
 
@@ -48,6 +48,9 @@ public final class BindingsCompetition {
         var sideboard = dc.SwitchBoard();
         var operator = dc.Operator();
 
+        var climber = RobotContainer.getSubsystem(Climber.class);
+        var shooter = RobotContainer.getSubsystem(ShooterServo.class);
+
         Trigger ManualShoot = sideboard.sw16();
         Trigger ClimberCalibrate = sideboard.sw11();
         Trigger ShooterCalibrate = sideboard.sw12();
@@ -70,17 +73,19 @@ public final class BindingsCompetition {
         ManualShoot.and(operator.rightBumper()).onTrue(new ShooterServoSequence(46.5, 2800.0));                                                                                                
         ManualShoot.and(operator.rightTrigger()).onTrue(new ShooterServoSequence(36.0, 3000.0)); // was 35
         // AutoShootm 
-        ManualShoot.negate().and(operator.rightBumper()).onTrue(new AutoShooting(ShootingTarget.Speaker, 45.0, 3000.0));
+        ManualShoot.negate().and(operator.rightBumper())
+            .onTrue(new AutoShooting(ShootingTarget.Speaker, 45.0, 3000.0));
         ManualShoot.negate().and(operator.rightTrigger())
-                .onTrue(new AutoShooting(ShootingTarget.Speaker, 36.0, 3200.0));
-        
+            .onTrue(new AutoShooting(ShootingTarget.Speaker, 36.0, 3200.0));
         
         // Calibration commands
         ShooterCalibrate.and(operator.povUp()).whileTrue(new ShooterAngleVelMove(2.0)); 
         ShooterCalibrate.and(operator.povDown()).whileTrue(new ShooterAngleVelMove(-2.0));
-        ShooterCalibrate.and(operator.povLeft().onTrue(new CalibrateSetZero()));
-        ClimberCalibrate.and(operator.povUp()).whileTrue(new ClimbVel(Climber.ClimbCalibrateVel));
-        ClimberCalibrate.and(operator.povDown().whileTrue(new ClimbVel(-Climber.ClimbCalibrateVel)));
-        ClimberCalibrate.and(operator.povLeft().onTrue(new ClimberCalibrate()));
+        ShooterCalibrate.and(operator.povLeft()).onTrue(
+            new InstantCommand( () -> { shooter.setExtensionPosition(0.0); } )); 
+        ClimberCalibrate.and(operator.povUp()).whileTrue(new ClimberVelocity(Climber.ClimbCalibrateVel));
+        ClimberCalibrate.and(operator.povDown()).whileTrue(new ClimberVelocity(-Climber.ClimbCalibrateVel));
+        ClimberCalibrate.and(operator.povLeft()).onTrue(
+            new InstantCommand( ()-> {climber.setClimberPos(0.0); } ));
     }
 }
