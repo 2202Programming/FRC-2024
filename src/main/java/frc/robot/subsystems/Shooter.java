f6e25139b4e7f27f04f6db8ed88c5acc1caf96b8
Noah Ren <nren2025@gmail.com>
2024-03-19 18:58:31 -0500

    forgot

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -29,8 +29,8 @@ public class Shooter extends SubsystemBase {
   final RelativeEncoder leftEncoder;
   final RelativeEncoder rightEncoder;
   final double FACTOR = 1.0;
-  final double left_kF = 1.0 / 5500.0;
-  final double right_kF = 1.0 / 5000.0;
+  final double left_kF = 1.0 / 5100.0;
+  final double right_kF = 1.0 / 4700-.0;
 
   private DoubleSolenoid shooterAngle; // can be replaced w/ servo in derived class
 
