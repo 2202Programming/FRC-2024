a079d5e2240b19f6d42c64bb40ffbeded2414f90
Beast Software <beastsoftwaredev@gmail.com>
2024-03-30 10:23:19 -0500

    bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -34,7 +34,7 @@ public class RobotContainer {
   }
 
   // Change the line below for testing, try not to commit a change 
-  public static final frc.robot.RobotContainer.Bindings bindings = Bindings.comp_not_comp;
+  public static final frc.robot.RobotContainer.Bindings bindings = Bindings.Competition;
 
   // The robot's subsystems and commands are defined here...
   static RobotContainer rc;
