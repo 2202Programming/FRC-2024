89c9da702775927f896815115961ca801fd96724
Noah Ren <nren2025@gmail.com>
2024-11-06 19:19:26 -0600

    set joystick

--- src/main/java/frc/robot/subsystems/hid/DriverControls.java
+++ src/main/java/frc/robot/subsystems/hid/DriverControls.java
@@ -7,7 +7,7 @@ public class DriverControls {
   }
 
   public enum Id {
-    Driver(0), Operator(1), SwitchBoard(2), Phantom(3),
+    Driver(0), Operator(1), SwitchBoard(2), JoystickOperator(3),
     // Just an ex. I dont know what Phantom is for lol.
     Joystick(4);
 
